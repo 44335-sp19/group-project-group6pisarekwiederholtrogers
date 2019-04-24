f53584a7dcb86555f01069e27d9cdf329896ef0f
Elizabeth Pisarek <s528849@nwmissouri.edu>
2019-04-23 23:51:23 -0500

    More javascript added
    Finally got the contact form to work or at least cooperate and added javascript alert button.

--- LizsCode/javascript.js
+++ LizsCode/javascript.js
@@ -1,4 +1,4 @@
-/*Social Media Shadow*/
+/*Social Media Shadow javascript*/
 
 function MouseOver(elem) {
   elem.style.boxShadow = "-10px -18px 6px gray";
@@ -6,4 +6,23 @@ function MouseOver(elem) {
 
 function MouseOut(elem) {
   elem.style.boxShadow = "none";
+}
+
+/*Slideshow jQuery*/
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(2000)
+    .end()
+    .appendTo('#slideshow');
+},  4000);
+
+/*Contact Form javascript*/
+
+function myFunction() {
+  alert("Thank you for your response!");
 }
