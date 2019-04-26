010e7573e8f0e8d01ba74847efc1da3973503d9c
Elizabeth Pisarek <s528849@nwmissouri.edu>
2019-04-26 11:36:49 -0500

    Logo jQuery added

--- homeJavascript.js
+++ homeJavascript.js
@@ -2,3 +2,15 @@
 function alertUser(msg) {
     alert(msg);
 }
+
+/*Liz's Logo Hover jquery*/
+$(document).ready(function() {
+    $('img').hover(			
+        function () {
+            $(this).css({"background-color":"#007bff"});
+               },	
+        function () {
+            $(this).css({"background-color":"white"});
+               }
+            );
+    });
